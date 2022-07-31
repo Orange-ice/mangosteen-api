e70a00e1187ba03eb6709a8be38e7e277742af78
Burt <2902978956@qq.com>
2022-07-31 21:35:51 +0800

    频繁发送验证码返回429

--- app/controllers/api/v1/validate_codes_controller.rb
+++ app/controllers/api/v1/validate_codes_controller.rb
@@ -1,5 +1,10 @@
 class Api::V1::ValidateCodesController < ApplicationController
   def create
+    if ValidateCode.exists?(email: params[:email], created_at: 1.minute.ago..Time.now)
+      render status: :too_many_requests
+      return
+    end
+
     validate_code = ValidateCode.new email: params[:email]
     if validate_code.save
       render status: :created
--- spec/requests/api/v1/validate_codes_spec.rb
+++ spec/requests/api/v1/validate_codes_spec.rb
@@ -2,9 +2,11 @@ require 'rails_helper'
 
 RSpec.describe "Api::V1::ValidateCodes", type: :request do
   describe "验证码" do
-    it "正常发送" do
+    it "发送太频繁会返回 429" do
       post api_v1_validate_codes_path, params: {email: "2902978956@qq.com"}
       expect(response).to have_http_status(:created)
+      post api_v1_validate_codes_path, params: {email: "2902978956@qq.com"}
+      expect(response).to have_http_status(:too_many_requests)
     end
   end
 end
