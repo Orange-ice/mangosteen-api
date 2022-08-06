5f22fcd2693d0a158ebc781382684270cfd31457
Burt <2902978956@qq.com>
2022-08-06 09:59:33 +0800

    支持按时间筛选账单

--- spec/requests/api/v1/bills_spec.rb
+++ spec/requests/api/v1/bills_spec.rb
@@ -32,5 +32,25 @@ RSpec.describe "Api::V1::Bills", type: :request do
       json = JSON.parse(response.body)
       expect(json['resources'].size).to eq(1)
     end
+
+    it "可按时间筛选" do
+      user = User.create email: '1@qq.com'
+      bill1 = Bill.create amount: 100, user_id: user.id, tag_id: 1, created_at: '2020-01-01'
+      bill2 = Bill.create amount: 100, user_id: user.id, tag_id: 1, created_at: '2020-01-05'
+      bill3 = Bill.create amount: 100, user_id: user.id, tag_id: 1, created_at: '2020-01-09'
+      get api_v1_bills_path, params: { start_date: '2020-01-01', end_date: '2020-01-06' }, headers: user.generate_auth_header
+      expect(response).to have_http_status(200)
+      json = JSON.parse(response.body)
+      expect(json['resources'].size).to eq(2)
+      expect(json['resources'][0]['id']).to eq(bill1.id)
+      expect(json['resources'][1]['id']).to eq(bill2.id)
+
+      # 边界条件
+      get api_v1_bills_path, params: { start_date: '2020-01-01', end_date: '2020-01-01' }, headers: user.generate_auth_header
+      expect(response).to have_http_status(200)
+      json = JSON.parse(response.body)
+      expect(json['resources'].size).to eq(1)
+      expect(json['resources'][0]['id']).to eq(bill1.id)
+    end
   end
 end
