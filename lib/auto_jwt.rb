20ea1562f2c158d95aa7aa6cdae8b18c404d5de7
Burt <2902978956@qq.com>
2022-08-06 15:18:09 +0800

    jwt 设置过期时间：2小时

--- lib/auto_jwt.rb
+++ lib/auto_jwt.rb
@@ -5,8 +5,18 @@ class AutoJwt
   end
 
   def call(env)
+    # 以下路径跳过 jwt 验证
+    white_list = ['/api/v1/session', '/api/v1/validate_codes']
+    return @app.call(env) if white_list.include?(env['PATH_INFO'])
+
     jwt = env['HTTP_AUTHORIZATION'].split(' ').last rescue ''
-    payload = JWT.decode(jwt, Rails.application.credentials.hmac_secret, true, algorithm: 'HS256') rescue nil
+    begin
+    payload = JWT.decode(jwt, Rails.application.credentials.hmac_secret, true, algorithm: 'HS256')
+    rescue JWT::ExpiredSignature
+      return [401, {}, [{ reason: 'token expired' }.to_json]]
+    rescue
+      return [401, {}, [{ reason: 'token invalid' }.to_json]]
+    end
     env['current_user_id'] = payload[0]['user_id'] rescue nil
     @app.call(env)
   end
--- spec/requests/api/v1/mes_spec.rb
+++ spec/requests/api/v1/mes_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
+require 'active_support/testing/time_helpers'
 
 RSpec.describe "Api::V1::Mes", type: :request do
+  include ActiveSupport::Testing::TimeHelpers
+
   describe "当前用户" do
     it "获取当前用户" do
       user = User.create email: '2902978956@qq.com'
@@ -11,6 +14,23 @@ RSpec.describe "Api::V1::Mes", type: :request do
       expect(response).to have_http_status(200)
       json = JSON.parse(response.body)
       expect(json['resource']['id']).to eq(user.id)
+    end
+
+    it "jwt 过期验证" do
+      user = User.create email: '1@qq.com'
+      header = user.generate_auth_header
+
+      # 1小时后，jwt未过期
+      travel_to 1.hour.from_now do
+        get api_v1_me_path, headers: header
+        expect(response).to have_http_status(200)
+      end
+
+      # 3小时后，jwt已过期
+      travel_to 3.hours.from_now do
+        get api_v1_me_path, headers: header
+        expect(response).to have_http_status(401)
+      end
     end
   end
 end
