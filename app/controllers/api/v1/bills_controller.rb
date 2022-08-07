2c3c57921f05ebe01428c5a8b3f4f7dd3eb2ffdf
Burt <2902978956@qq.com>
2022-08-07 21:05:07 +0800

    账单根据时间查询改为查询 happened_at 字段

--- app/controllers/api/v1/bills_controller.rb
+++ app/controllers/api/v1/bills_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::BillsController < ApplicationController
   def index
     user_id = request.env['current_user_id']
     return render status: :unauthorized unless user_id
-    bills = Bill.where(user_id: user_id, created_at: params[:start_date]..params[:end_date])
+    bills = Bill.where(user_id: user_id, happened_at: params[:start_date]..params[:end_date])
       .page(params[:page]).per(params[:per_page])
     
     render status: :ok, json: { resources: bills, pager: {
